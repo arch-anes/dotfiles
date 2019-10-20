feee052329e138fe2f90bbe112c35d2ffcfb5a69
Anes Belfodil <ans.belfodil@gmail.com>
2019-10-20 14:18:11 -0400

    Switch to wayland

--- home/.config/fish/conf.d/01_login.fish
+++ home/.config/fish/conf.d/01_login.fish
@@ -2,7 +2,6 @@ if status --is-login
   gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
 
   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-    # exec ssh-agent sway
-    exec ssh-agent startx -- -keeptty
+    exec ssh-agent sway
   end
 end
