f349646602ffa4fec53b7531fbb138ba77840e80
Anes Belfodil <ans.belfodil@gmail.com>
2020-05-02 22:04:40 -0400

    Install needed packages

--- setup.fish
+++ setup.fish
@@ -2,18 +2,48 @@
 
 set source_dir (dirname (readlink -m (status --current-filename)))
 
+set enable_services systemctl --now enable
+set install_packages yay -Syu --needed --noeditmenu --nodiffmenu --noconfirm
+
 # Git
 stow hooks -t $source_dir/.git/hooks -R -d $source_dir
 git -C $source_dir submodule update --init --recursive
 curl -sLf https://raw.githubusercontent.com/ngerakines/commitment/master/commit_messages.txt -o $HOME/.cache/commit_messages.txt
 
-# Pre-setup
+# Packages
+
+sudo pacman -Syu --needed --noconfirm yay
+
+$install_packages (cat packages/*.pkg)
+
+set VGA (lspci | grep VGA)
+switch $VGA
+    case "*AMD*"
+        $install_packages amdgpu-fan radeon-profile-daemon-git
+        $enable_services amdgpu-fan.service radeon-profile-daemon.service
+    case "*NVIDIA*"
+        $install_packages nvidia nvidia-utils lib32-nvidia-utils nvidia-settings
+    case '*'
+        echo -n "Unknown graphics card"
+end
+
+# Services
+
+$enable_services syncthing@$USER.service
+$enable_services docker.service
+$enable_services --user mpd.service
+
+sudo ufw enable
+
+sudo gpasswd -a $USER docker
+
+# Symlinks
+
 sudo rm -rf /etc/ssh/sshd_config
 set CONFIG_DIR $HOME/.config
 rm -rf $CONFIG_DIR/fish
 mkdir -p $HOME/.ssh
 
-# Symlinks
 stow csgo -t "$HOME/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/csgo" -R -d $source_dir ^/dev/null >/dev/null
 stow home -t $HOME -R -d $source_dir
 sudo stow etc -t /etc -R -d $source_dir
