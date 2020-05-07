bf0608714be910ce396b25e532d4723068ee2f3b
Anes Belfodil <ans.belfodil@gmail.com>
2020-05-07 00:22:49 -0400

    Remove useless aliases

--- home/.config/fish/conf.d/02_alias.fish
+++ home/.config/fish/conf.d/02_alias.fish
@@ -5,10 +5,8 @@ alias fulllistpkg='expac --timefmt="%Y-%m-%d %T" "%l  %w\t%-20n\t%10d" (pacman -
 alias fix-pacman-db='sudo rm /var/lib/pacman/db.lck'
 alias gensrcinfo='makepkg --printsrcinfo > .SRCINFO'
 alias listpkg='expac --timefmt="%Y-%m-%d %T" "%l  %w\t%-20n\t%10d" (comm -23 (pacman -Qq | sort | psub) (pacman -Qqg base-devel xorg xorg-apps | sort | psub)) | sort -n'
-alias rankmirrors-top="curl -s 'https://www.archlinux.org/mirrorlist/?country=CA&country=US&use_mirror_status=on' | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 10 - | sudo tee /etc/pacman.d/mirrorlist"
 alias remove='yay -Rsn'
 alias update='yay -Syu --needed --noeditmenu --nodiffmenu'
-alias updatel='update --ignore="linux*"'
 
 # Git
 alias swag='gaa && yolo && gl && gp'
@@ -20,13 +18,9 @@ alias pcbcom='picocom /dev/ttyACM0 -b 921600 --imap lfcrlf'
 
 # Other
 alias broken-ln='find -xtype l -print'
-alias btc='bluetoothctl'
 alias docker-prune='docker system prune --all --volumes'
 alias id-key='xev | awk -F'"'"'[ )]+'"'"' '"'"'/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'"'"
-alias fixbt='sudo rfkill unblock all'
-alias fixwifi='sudo modprobe -r 8821ce && sudo modprobe 8821ce && sudo ip link set (ls /sys/class/net | grep wl) up'
 alias lvl='echo $SHLVL'
-alias music='mpc update && ncmpcpp'
 alias sort-dir='find ./ -type f -exec sort -o {} {} \;'
 alias unmount='udiskie-umount'
 alias update-node='yarn global upgrade'
