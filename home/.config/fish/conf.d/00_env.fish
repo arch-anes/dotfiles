e880342a9d97024ea88bd70a94105719519d63b9
Anes Belfodil <ans.belfodil@gmail.com>
2020-08-22 16:54:07 -0400

    Remove .net

--- home/.config/fish/conf.d/00_env.fish
+++ home/.config/fish/conf.d/00_env.fish
@@ -6,11 +6,6 @@ set -xg ANDROID_HOME $HOME/Android/Sdk
 set -xg PATH $PATH $ANDROID_HOME/tools
 set -xg PATH $PATH $ANDROID_HOME/platform-tools
 
-# .NET
-set -xg DOTNET_CLI_TELEMETRY_OPTOUT 1
-set -xg DOTNET_ROOT /opt/dotnet
-set -xg PATH $PATH $HOME/.dotnet/tools
-
 # Vim
 set -xg SPACEVIMDIR $HOME/.config/spacevim/
 set -xg PATH $PATH $HOME/.SpaceVim/bin
