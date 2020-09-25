98449bfa1f5d60dd4d4cf645f627f0257fb92df3
Anes Belfodil <ans.belfodil@gmail.com>
2020-09-25 12:03:01 -0400

    Update aliases

--- home/.config/fish/conf.d/02_alias.fish
+++ home/.config/fish/conf.d/02_alias.fish
@@ -1,16 +1,14 @@
 # Arch Linux
-alias add-key='gpg --recv-keys'
-alias cleanup='yay -Yc && yay -Sc'
-alias fulllistpkg='expac --timefmt="%Y-%m-%d %T" "%l  %w\t%-20n\t%10d" (pacman -Qq) | sort -n'
+alias cleanup='yay -Yc && yay -Scc'
 alias fix-pacman-db='sudo rm /var/lib/pacman/db.lck'
 alias gensrcinfo='makepkg --printsrcinfo > .SRCINFO'
-alias listpkg='expac --timefmt="%Y-%m-%d %T" "%l  %w\t%-20n\t%10d" (comm -23 (pacman -Qq | sort | psub) (pacman -Qqg base-devel xorg xorg-apps | sort | psub)) | sort -n'
+alias listpkg='expac --timefmt="%Y-%m-%d %T" "%l  %w\t%-20n\t%10d" (pacman -Qq) | sort -n'
 alias remove='yay -Rsn'
-alias update='yay -Syu --needed --noeditmenu --nodiffmenu'
+alias update='yay -Syu --devel --needed --noeditmenu --nodiffmenu --sudoloop'
 
 # Git
-alias swag='gaa && yolo && gl && gp'
-alias yolo='gc -m (shuf -n 1 $HOME/.cache/commit_messages.txt)'
+alias yolo='git commit -m (shuf -n 1 $HOME/.cache/commit_messages.txt)'
+alias swag='git add --all && yolo && git pull && git push'
 
 # Embedded dev
 alias listports='dmesg | grep tty'
@@ -21,7 +19,6 @@ alias broken-ln='find -xtype l -print'
 alias docker-prune='docker system prune --all --volumes'
 alias lvl='echo $SHLVL'
 alias sort-dir='find . -type f ! -path \'*/.git/*\' -exec sort -o {} {} \;'
-alias update-node='yarn global upgrade'
 alias watchdir='inotifywait -r -m . --format "%w%f %e"'
 
 alias du='du -h'
