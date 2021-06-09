80b1d337a985d14d35b4b00bedd249be2fcc1a6d
Anes Belfodil <ans.belfodil@gmail.com>
2021-06-09 09:05:14 -0400

    Create config dir if doesn't exist

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pushd ~/.config
+config_dir="$HOME/.config"
+mkdir -p $config_dir
+pushd $config_dir
 curl https://codeload.github.com/arch-anes/dotfiles/zip/master -so dotfiles-master.zip
 unzip -qo dotfiles-master.zip
 rm -f dotfiles-master.zip
