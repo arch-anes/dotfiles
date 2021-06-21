b54fd9e9201271df01f12f03aa2d0efaae2e6655
Anes Belfodil <ans.belfodil@gmail.com>
2021-06-21 17:28:20 -0400

    Automatically install unzip dependency

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ "$(cat /etc/os-release | grep manjaro)" ] && sudo pacman -Syu --needed --noconfirm unzip
+[ "$(cat /etc/os-release | grep ubuntu)" ] && sudo apt update && sudo apt install -y unzip
+
 config_dir="$HOME/.config"
 mkdir -p $config_dir
 pushd $config_dir
