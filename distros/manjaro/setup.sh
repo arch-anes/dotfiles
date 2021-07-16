9b582d0768f3c7bb6fae735e6b733313311219c7
Anes Belfodil <ans.belfodil@gmail.com>
2021-07-16 08:40:04 -0400

    Don't ask for confirmation when removing packages in arch

--- distros/manjaro/setup.sh
+++ distros/manjaro/setup.sh
@@ -8,10 +8,10 @@ fi
 
 source_dir=$(dirname "$(readlink -f "$0")")
 
-install_packages="yay -Syu --needed --noeditmenu --nodiffmenu --noconfirm --sudoloop"
-remove_packages="yay -Rs"
-
 sudo pacman -Syu --needed --noconfirm yay
+
+install_packages="yay -Syu --needed --noeditmenu --nodiffmenu --noconfirm --sudoloop"
+remove_packages="yay -Rs --noconfirm"
 
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8FD3D9A8D3800305A9FFF259D1742AD60D811D58
 gpg --keyserver pool.sks-keyservers.net --recv-keys B4322F04D67658D8
