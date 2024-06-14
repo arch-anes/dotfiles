3e90c32a18fba70ca0ff940375de9e6c88a3f382
Anes Belfodil <ans.belfodil@gmail.com>
2024-06-13 21:18:34 -0400

    Replace pip with pipx

--- distros/ubuntu/setup.sh
+++ distros/ubuntu/setup.sh
@@ -26,7 +26,7 @@ sudo -E add-apt-repository -y ppa:fish-shell/release-3
 sudo -E apt-key adv --refresh-keys --keyserver keyserver.ubuntu.com
 
 sudo -E apt update && sudo -E apt install -y $(cat $source_dir/packages/base)
-sudo pip3 install thefuck
+sudo PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install --include-deps thefuck
 
 curl -Ssf https://pkgx.sh | sh
 
