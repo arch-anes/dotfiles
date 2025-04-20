678f42046b54b95a37b5edd2bf83a21e1e32bd9f
Anes Belfodil <ans.belfodil@gmail.com>
2025-04-20 01:03:52 -0400

    Remove raspi brew workaround since brew now supports linux arm

--- distros/ubuntu/setup.sh
+++ distros/ubuntu/setup.sh
@@ -22,19 +22,9 @@ sudo -E apt install -y $(cat $source_dir/packages/ubuntu)
 
 NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-if command -v /home/linuxbrew/.linuxbrew/bin/brew &> /dev/null; then
-    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    brew install $(cat $source_dir/packages/brew)
-    sudo ln -s /home/linuxbrew/.linuxbrew/bin/fish /usr/bin/fish
-else
-    # Workaround for the absence of Homebrew on Raspberry Pi
-    sudo -E add-apt-repository -y ppa:fish-shell/release-3
-    sudo -E apt update && sudo -E apt install -y fish rustup cmake
-    rustup default stable
-    cargo install starship zoxide --locked
-    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
-fi
-
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+brew install $(cat $source_dir/packages/brew)
+sudo ln -s /home/linuxbrew/.linuxbrew/bin/fish /usr/bin/fish
 
 ##############
 ### Config ###
