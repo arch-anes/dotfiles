48974a3bc82c3514dd0c032c6b760680fbe7b34b
Anes Belfodil <ans.belfodil@gmail.com>
2025-08-01 20:33:08 -0400

    Add openwrt support
    Closes #12

---
+++ distros/openwrt/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+############
+### Vars ###
+############
+is_ubuntu="$(cat /etc/os-release | grep openwrt)"
+if [ ! "$is_ubuntu" ]; then
+    echo "Not OpenWRT. Skipping package installation."
+    exit 0
+fi
+
+source_dir=$(dirname "$(readlink -f "$0")")
+
+################
+### Packages ###
+################
+opkg update && opkg install $(cat $source_dir/packages/ubuntu)
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 source_dir=$(pushd $(dirname $0) >/dev/null && pwd && popd >/dev/null)
 
 $source_dir/distros/arch/setup.sh
+$source_dir/distros/openwrt/setup.sh
 $source_dir/distros/ubuntu/setup.sh
 
 $source_dir/distros/macos/setup.sh
